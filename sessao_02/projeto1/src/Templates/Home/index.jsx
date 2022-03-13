768473e350e019bb9604d905ceeb2b309855aa70
CLAUDISNEI <claudisneibello@gmail.com>
2022-03-12 22:04:38 -0300

    Alterado a classe Home para função

--- sessao_02/projeto1/src/Templates/Home/index.jsx
+++ sessao_02/projeto1/src/Templates/Home/index.jsx
@@ -1,7 +1,8 @@
 
 import './styles.css';
 
-import {Component} from 'react';
+import {useState,useEffect, useCallback} from 'react';
+
 
 //importando os dados 
 import {loadPosts} from '../../utils/load-posts';
@@ -11,126 +12,129 @@ import { Posts } from '../../Components/Posts';
 import { Button } from '../../Components/Button';
 import {TextInput} from '../../Components/TextInput';
 
-class Home extends Component{
- 
-    state ={
+export const Home = () => {
+  
+  /** state ={
       posts:[],
       allPosts:[],
       page:0,
       postsPerPage: 10,
       searchValue: ''
     };
+ */
+  
+  //useState das variáveis
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(10);
+  const [searchValue, setSearchValue] = useState('');
 
-    //esse é um life cicle metods
-    async componentDidMount(){
-      await this.loadPosts();
-    }
+  /*iremos determinar se existe mais posts para serem mostrados na página
+  para isso vamos verificar se a página que estamos mais quantidade de posts por
+  página for maior ou igual a quantidade total de posts, significa que
+  não temos mais páginas para ir.
+  vamos passar então a propriedade noMorePosts para o atributo disabled.
+  */
+ const noMorePosts = page + postsPerPage >= allPosts.length;
 
-    loadPosts = async () =>{
-      const postsAndPhotos = await loadPosts();
-      const {page, postsPerPage}= this.state;
-      //susbstituimos o postJson que tinha somente os posts pelo postsAndPhotos
-      this.setState({
-        posts: postsAndPhotos.slice(page,postsPerPage),
-        allPosts: postsAndPhotos
-      });
-    }
+  //variável utilizada para filtrar os posts conforme conteúdo do input
+  const filteredPosts = !!searchValue ? 
+  allPosts.filter(post => {
+    return post.title.toLowerCase().includes(
+      searchValue.toLowerCase()
+    )
+  })
+  : posts ;
 
-    //carregando mais posts na página
-    loadMorePosts = () =>{
-      //carrega as variaveis que iremos precisar do state
-     const {
-       page,
-       postsPerPage,
-       allPosts,
-       posts
-     } = this.state;
+  
 
-     //nextPage terá a página mais a quantidade de postes por página
-     const nextPage = page + postsPerPage;
+  const handleLoadPosts = useCallback(async (page, postsPerPage) =>{
+    const postsAndPhotos = await loadPosts();
+   // const {page, postsPerPage}= this.state;
 
-     //criando um novo array que terá somente os posts que farão parte
-     //da página que será mostrada
-     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage)
+    //susbstituimos o postJson que tinha somente os posts pelo postsAndPhotos    
+    setPosts(postsAndPhotos.slice(page,postsPerPage));
+    setAllPosts(postsAndPhotos);
 
-     //passamos para posts o array somente com os itens que serão mostrados
-     posts.push(...nextPosts);
+  },[])
 
-     //setamos agora no state o novo array de posts e a nova pagina
-     this.setState({posts, page: nextPage});
-     console.log(page, postsPerPage, nextPage, nextPage + postsPerPage);
-    }
+  //utilizando um hook 
+ useEffect(()=>{
+  handleLoadPosts(0, postsPerPage);
+}, [handleLoadPosts, postsPerPage]);
+  
+  
 
-    //método para realizar busca no input que recebe o evento
-    handleChange = (e)=>{
-      //destruct capturando o target do evento
-      const{value} = e.target;
 
-      //passamos o novo valor de searchValue utilizando o setState
-      this.setState({searchValue: value});
-    }
+  //carregando mais posts na página
+  const loadMorePosts = () =>{
+  
+   //nextPage terá a página mais a quantidade de postes por página
+   const nextPage = page + postsPerPage;
 
-  render(){
-    const {posts, page, postsPerPage, allPosts, searchValue } = this.state;
+   //criando um novo array que terá somente os posts que farão parte
+   //da página que será mostrada
+   const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage)
 
-    //variável utilizada para filtrar os posts conforme conteúdo do input
-    const filteredPosts = !!searchValue ? 
-    allPosts.filter(post => {
-      return post.title.toLowerCase().includes(
-        searchValue.toLowerCase()
-      )
-    })
-    : posts ;
+   //passamos para posts o array somente com os itens que serão mostrados
+   posts.push(...nextPosts);
 
-    /*iremos determinar se existe mais posts para serem mostrados na página
-    para isso vamos verificar se a página que estamos mais quantidade de posts por
-    página for maior ou igual a quantidade total de posts, significa que
-    não temos mais páginas para ir.
-    vamos passar então a propriedade noMorePosts para o atributo disabled.
-    */
-   const noMorePosts = page + postsPerPage >= allPosts.length;
+   //setamos agora no state o novo array de posts e a nova pagina
+   setPosts(posts);
+   setPage(nextPage);
 
-    return (
-      <section className='container'>
-        
-        {!!searchValue && (
-          <div className='search-container'>
-            <h1>Search value: {searchValue} Qtd posts: {filteredPosts.length}</h1>
-          </div>
-        )}
-        
-        <TextInput
-          onChange={this.handleChange}
-          type='search' 
-          value={searchValue}
-          placeholder='Type your search'
-        />  
-        
-        {filteredPosts.length >0 &&(
-           <Posts posts={filteredPosts} />
-        )}
-
-        {filteredPosts.length ===0 &&(
-          <p>Não existem posts :( </p>
-          
-        )}
-       
-
-        <div className='button-container'>
-          
-          {!searchValue &&(
-            <Button 
-            text='Load more posts'
-            onClick={this.loadMorePosts}
-            disabled={noMorePosts}
-          />
-          )}
-
-        </div>
-      </section>
-      
-    );
   }
-}
 
-export default Home;
+  //método para realizar busca no input que recebe o evento
+ const handleChange = (e)=>{
+    //destruct capturando o target do evento
+    const{value} = e.target;
+    //passamos o novo valor de searchValue utilizando a função setSearchValue
+    setSearchValue(value);
+  }
+
+  
+
+  return (
+    <section className='container'>
+      
+      {!!searchValue && (
+        <div className='search-container'>
+          <h1>Search value: {searchValue} Qtd posts: {filteredPosts.length}</h1>
+        </div>
+      )}
+      
+      <TextInput
+        onChange={handleChange}
+        type='search' 
+        value={searchValue}
+        placeholder='Type your search'
+      />  
+      
+      {filteredPosts.length >0 &&(
+         <Posts posts={filteredPosts} />
+      )}
+
+      {filteredPosts.length ===0 &&(
+        <p>Não existem posts :( </p>
+        
+      )}
+     
+
+      <div className='button-container'>
+        
+        {!searchValue &&(
+          <Button 
+          text='Load more posts'
+          onClick={loadMorePosts}
+          disabled={noMorePosts}
+        />
+        )}
+
+      </div>
+    </section>
+    
+  );
+
+}
